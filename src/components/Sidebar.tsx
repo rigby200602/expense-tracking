f2870a814f7a867c10f71e86cd4f0c7d459d0cbe
rigby200602 <tungle3456@gmail.com>
2026-06-29 23:06:53 +0700

    add logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,11 @@
-
+import logo from "../assets/logo.png"
 
 const Sidebar = () => {
   return (
     <aside className="h-screen">
         <nav className="h-full flex flex-col border-r border-white shadow-sm">
             <div className="p-4 pb-2 flex justify-between items-center">
-                <p className="text-black">FinTracker</p>
+                <img src={logo} className="w-12"/>
             </div>
         </nav>
     </aside>
