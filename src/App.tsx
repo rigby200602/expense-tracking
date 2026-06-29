d9728e4cd7c651cfdc7d062d5951a6e394aad388
rigby200602 <tungle3456@gmail.com>
2026-06-29 22:38:16 +0700

    add Sidebar to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Sidebar from "./components/Sidebar"
 
 
 function App() {
 
   return (
     <>
-      <h1 className='text-cyan-700 text-2xl'>Hello World!</h1>
+      <Sidebar></Sidebar>
     </>
   )
 }
