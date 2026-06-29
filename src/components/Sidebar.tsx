dcaeaffbba80496731bfed8db2e0fc9416735e24
rigby200602 <tungle3456@gmail.com>
2026-06-29 23:28:54 +0700

    add span

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,8 +4,9 @@ const Sidebar = () => {
   return (
     <aside className="h-screen">
         <nav className="h-full flex flex-col border-r border-white shadow-sm">
-            <div className="p-4 pb-2 flex justify-between items-center">
-                <img src={logo} className="w-12"/>
+            <div className="p-4 pb-2 flex items-center gap-3">
+              <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
+              <span className="text-lg font-semibold">FinTracker</span>
             </div>
         </nav>
     </aside>
