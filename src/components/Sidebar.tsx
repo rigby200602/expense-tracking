1361f2d66b13ac499f64a0686d173b7ba745c04a
rigby200602 <tungle3456@gmail.com>
2026-06-29 23:51:14 +0700

    update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
     <aside className="h-screen">
         <nav className="h-full flex flex-col border-r bg-white shadow-sm">
             <div className="p-4 pb-2 flex items-center gap-3">
-              <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
+              <img src={logo} alt="logo" className="w-12" />
               <span className="text-lg font-semibold">FinTracker</span>
             </div>
         </nav>
