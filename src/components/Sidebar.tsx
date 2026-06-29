65980da2244f4ff52d4ee56784d6da677ff6d746
rigby200602 <tungle3456@gmail.com>
2026-06-29 22:48:56 +0700

    start building sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,13 @@
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside className="h-screen">
+        <nav className="h-full flex flex-col border-r border-white shadow-sm">
+            <div className="p-4 pb-2 flex justify-between items-center">
+                <p className="text-black">FinTracker</p>
+            </div>
+        </nav>
+    </aside>
   )
 }
 
