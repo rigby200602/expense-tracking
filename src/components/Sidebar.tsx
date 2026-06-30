e92d6ba598f96842ef203dc675ce02b6f6faace5
rigby200602 <tungle3456@gmail.com>
2026-06-30 20:52:42 +0700

    collaspe logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,7 +16,9 @@ const Sidebar = ({children} : SidebarProps ) => {
     <div className="h-screen">
       <div className="max-w-1/6 h-full flex flex-col border-r border-gray-700 bg-[#0A0F16] shadow-sm">
         <div className="p-4 pb-2 flex items-center gap-3 ">
-          <img src="/src/assets/logo.png" alt="logo" className="w-12 cursor-pointer" />
+          <img src="/src/assets/logo.png" alt="logo" className={
+            `overflow-hidden transition-all duration-300 ${isCollapsed ? "w-0" : "w-12"}`
+          }/>
           <span className="text-lg text-white font-semibold cursor-pointer">
             FinTracker
           </span>
