ef6a05d1c9e051ad85663ca574ba12207ced143a
rigby200602 <tungle3456@gmail.com>
2026-06-30 19:13:09 +0700

    update background color to index and sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,10 +3,10 @@ import logo from "../assets/logo.png"
 const Sidebar = () => {
   return (
     <aside className="h-screen">
-        <nav className="max-w-1/6 h-full flex flex-col border-r bg-white shadow-sm">
+        <nav className="max-w-1/6 h-full flex flex-col border-r bg-[#0A0F16] shadow-sm">
             <div className="p-4 pb-2 flex items-center gap-3">
               <img src={logo} alt="logo" className="w-12" />
-              <span className="text-lg font-semibold">FinTracker</span>
+              <span className="text-lg text-white font-semibold">FinTracker</span>
             </div>
         </nav>
     </aside>
