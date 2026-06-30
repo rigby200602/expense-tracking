0374544aca80cfe4ea5d526810ea42df5a0d311c
rigby200602 <tungle3456@gmail.com>
2026-06-30 19:18:28 +0700

    update pointer 2

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,9 +4,9 @@ const Sidebar = () => {
   return (
     <aside className="h-screen">
         <nav className="max-w-1/6 h-full flex flex-col border-r border-gray-700 bg-[#0A0F16] shadow-sm">
-            <div className="p-4 pb-2 flex items-center gap-3 cursor-pointer">
-              <img src={logo} alt="logo" className="w-12" />
-              <span className="text-lg text-white font-semibold">FinTracker</span>
+            <div className="p-4 pb-2 flex items-center gap-3 ">
+              <img src={logo} alt="logo" className="w-12 cursor-pointer" />
+              <span className="text-lg text-white font-semibold cursor-pointer">FinTracker</span>
             </div>
         </nav>
     </aside>
