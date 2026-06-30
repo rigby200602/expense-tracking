db76d0f4a7a6883602050b32d66979244cfa717c
rigby200602 <tungle3456@gmail.com>
2026-06-30 19:21:28 +0700

    update function

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,18 @@
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 
-const Sidebar = () => {
+const Sidebar = ({}) => {
   return (
-    <aside className="h-screen">
-        <nav className="max-w-1/6 h-full flex flex-col border-r border-gray-700 bg-[#0A0F16] shadow-sm">
-            <div className="p-4 pb-2 flex items-center gap-3 ">
-              <img src={logo} alt="logo" className="w-12 cursor-pointer" />
-              <span className="text-lg text-white font-semibold cursor-pointer">FinTracker</span>
-            </div>
-        </nav>
-    </aside>
-  )
-}
+    <div className="h-screen">
+      <div className="max-w-1/6 h-full flex flex-col border-r border-gray-700 bg-[#0A0F16] shadow-sm">
+        <div className="p-4 pb-2 flex items-center gap-3 ">
+          <img src={logo} alt="logo" className="w-12 cursor-pointer" />
+          <span className="text-lg text-white font-semibold cursor-pointer">
+            FinTracker
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
