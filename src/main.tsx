7f4f41b900d4f8619530e14bc6b95e16baa49c9a
rigby200602 <tungle3456@gmail.com>
2026-07-02 21:53:47 +0700

    refactor: add nested route for Home component in main application

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import { AppProvider } from "./context/AppContext.tsx";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </AppProvider>
