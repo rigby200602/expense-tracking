8c408e61473ddd7b687a5cfa0a4cbf846b615887
rigby200602 <tungle3456@gmail.com>
2026-07-02 22:29:21 +0700

    fix: adjust Navbar component to properly include Login link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { Link } from "react-router";
 
 const Navbar = () => {
     const [signIn, setSignIn] = useState(false);
   return <div className="absolute right-0 pr-[1%]">
         <div>
-
+            <Link to="/login">Login</Link>
         </div>
     </div>;
 };
