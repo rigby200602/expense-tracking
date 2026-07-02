027fd8c6fd52aa5c190b5bdfc2ae2774ea434b58
rigby200602 <tungle3456@gmail.com>
2026-07-02 23:00:06 +0700

    fix: correct login link rendering logic in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { Link } from "react-router";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
   const [logIn, setLogIn] = useState(false);
   return (
     <div className="absolute right-0 pr-[1%]">
-      <div>{logIn ? <Link to="/login">Login</Link> : 
       <div>
-        
+        {!logIn ? <Link to="/login">Login</Link> : 
+      <div>
+        <FaRegUserCircle className="text-3xl cursor-pointer" />
     </div>}</div>
     </div>
   );
