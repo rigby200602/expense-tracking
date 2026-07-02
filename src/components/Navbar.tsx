667f42636e1a422a00443f69f960b6ca0b9d2f99
rigby200602 <tungle3456@gmail.com>
2026-07-02 22:31:33 +0700

    style: format Navbar component for improved readability

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { Link } from "react-router";
 
 const Navbar = () => {
-    const [signIn, setSignIn] = useState(false);
-  return <div className="absolute right-0 pr-[1%]">
-        <div>
-            <Link to="/login">Login</Link>
-        </div>
-    </div>;
+  const [signIn, setSignIn] = useState(false);
+  return (
+    <div className="absolute right-0 pr-[1%]">
+      <div>{!signIn ? <Link to="/login">Login</Link> : <></>}</div>
+    </div>
+  );
 };
 
 export default Navbar;
