2f17c09f89a82307700b82ff13c01455834feeae
rigby200602 <tungle3456@gmail.com>
2026-07-02 21:31:11 +0700

    refactor: integrate React Router for routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AppProvider } from './context/AppContext.tsx'
+import { StrictMode } from "react";
+import "./index.css";
+import App from "./App.tsx";
+import { AppProvider } from "./context/AppContext.tsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </AppProvider>
   </StrictMode>,
-)
+);
