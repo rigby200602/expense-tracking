7f26bf51592c866b3c222b69f3d490b605f9ce37
rigby200602 <tungle3456@gmail.com>
2026-07-02 22:38:28 +0700

    fix: correct variable name in Navbar component and adjust login link rendering logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import { Link } from "react-router";
 
 const Navbar = () => {
-  const [signIn, setSignIn] = useState(false);
+  const [logIn, setLogIn] = useState(false);
   return (
     <div className="absolute right-0 pr-[1%]">
-      <div>{!signIn ? <Link to="/login">Login</Link> : <></>}</div>
+      <div>{logIn ? <Link to="/login">Login</Link> : 
+      <div>
+        
+    </div>}</div>
     </div>
   );
 };
