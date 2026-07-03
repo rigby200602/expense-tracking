7c7a76ea3d8b9839fe415b3f18c5bcf4ffd4d055
rigby200602 <tungle3456@gmail.com>
2026-07-03 22:25:31 +0700

    fix: add dropdown functionality to Navbar with isOpen state management

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router";
-import { FaRegUserCircle, FaArrowDown  } from "react-icons/fa";
+import { FaRegUserCircle, FaArrowDown } from "react-icons/fa";
 import { useAppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const { logIn } = useAppContext();
+  const { logIn, isOpen, setIsOpen, setLogIn } = useAppContext();
 
   return (
     <div className="absolute right-0 pr-[1%]">
@@ -17,9 +17,27 @@ const Navbar = () => {
             <FaRegUserCircle className="text-3xl" />
             <p>Le Dien Thanh Tung</p>
             <div className="text-white">
-              <button className="cursor-pointer">
-                <FaArrowDown className="text-xl my-1"/>
+              <button
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                  console.log(isOpen);
+                }}
+                className="cursor-pointer"
+              >
+                <FaArrowDown className="flex flex-col text-xl my-1" />
               </button>
+                <ul
+                  className={`absolute right-0 w-24 border rounded-2xl border-gray-700 bg-gray-800 p-2 mx-2 text-center
+                transition-all duration-300 ease-in-out
+                ${
+                  isOpen
+                    ? "translate-y-0 opacity-100 visible"
+                    : "-translate-y-2 opacity-0 invisible pointer-events-none"
+                }`}
+                >
+                  <li className="hover:text-blue-400">Profile</li>
+                  <li className="hover:text-blue-400">Logout</li>
+                </ul>
             </div>
           </div>
         )}
