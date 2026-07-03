e9c50dfb7257c7c2919b66072225bde8eb96bd7a
rigby200602 <tungle3456@gmail.com>
2026-07-03 20:58:35 +0700

    style: improve formatting of conditional rendering in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,23 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { useAppContext } from "../context/AppContext";
 
 const Navbar = () => {
-    const { logIn } = useAppContext();
+  const { logIn } = useAppContext();
 
   return (
     <div className="absolute right-0 pr-[1%]">
       <div>
-        {logIn ? 
-        <Link className="" to="/login">Login</Link> : 
-      <div className="flex gap-2 font-medium cursor-pointer">
-        <FaRegUserCircle className="text-3xl" />
-        <p>Le Dien Thanh Tung</p>
-    </div>}</div>
+        {logIn ? (
+          <Link className="" to="/login">
+            Login
+          </Link>
+        ) : (
+          <div className="flex gap-2 font-medium cursor-pointer">
+            <FaRegUserCircle className="text-3xl" />
+            <p>Le Dien Thanh Tung</p>
+            
+          </div>
+        )}
+      </div>
     </div>
   );
 };
