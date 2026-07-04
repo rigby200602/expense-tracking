ba0385973dbcbe78eebd239b2a448b49cf90d3f0
rigby200602 <tungle3456@gmail.com>
2026-07-04 19:51:35 +0700

    fix: update Home component layout to display total balance card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,11 @@
 
 const Home = () => {
   return (
-    <div className="text-white"></div>
+    <div className="text-white flex gap-4 flex-wrap">
+      <div className="w-[24%] h-32 bg-gray-800 rounded-3xl p-4 mt-[7%]">
+        <p>Total balance</p>
+      </div>
+    </div>
   )
 }
 
