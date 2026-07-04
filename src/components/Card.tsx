99c0eeff68d55e5787a5c654f5559671f3cb2399
rigby200602 <tungle3456@gmail.com>
2026-07-04 20:36:07 +0700

    fix: add min-width to Card component for better layout consistency

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import { MdAccountBalanceWallet } from "react-icons/md";
 
 const Card = () => {
   return (
-    <div className="flex h-32 bg-gray-800 rounded-3xl p-4">
+    <div className="flex min-w-52 h-32 bg-gray-800 rounded-3xl p-4">
         <div className="flex flex-col mx-2">
           <p className="text-gray-400">Total balance</p>
           <p className="text-white font-medium text-4xl">40000</p>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,10 @@ import Card from "../components/Card";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-4 gap-4 flex-wrap mt-15">
-      <Card/>
+    <div>
+      <div className="grid grid-cols-4 gap-4 flex-wrap mt-15">
+        <Card/>
+      </div>
     </div>
   );
 };
