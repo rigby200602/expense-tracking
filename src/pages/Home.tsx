edeb6a65f20fe9d04693b5cb2c8d6d64407450fb
rigby200602 <tungle3456@gmail.com>
2026-07-04 23:02:15 +0700

    fix: update key prop in Home component's Card mapping to use label

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,8 +13,8 @@ const Home = () => {
   return (
     <div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 flex-wrap mt-15">
-        {data.map((item, index) => (
-          <Card key={index} label={item.label} num={item.num} Icon={item.Icon} />
+        {data.map((item) => (
+          <Card key={item.label} label={item.label} num={item.num} Icon={item.Icon} />
         ))}
       </div>
     </div>
