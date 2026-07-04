c27bb7fd3288d32b78559ddff3f7ead586903b8c
rigby200602 <tungle3456@gmail.com>
2026-07-04 20:43:56 +0700

    fix: update grid layout in Home component for better responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import Card from "../components/Card";
 const Home = () => {
   return (
     <div>
-      <div className="grid grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 flex-wrap mt-15">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 flex-wrap mt-15">
         <Card/>
         <Card/>
         <Card/>
