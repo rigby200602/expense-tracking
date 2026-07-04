9c3c6d914b755ed16c38f9aee8ab5d7c17a12548
rigby200602 <tungle3456@gmail.com>
2026-07-04 20:26:04 +0700

    feat: add Card component to display total balance

---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import { MdAccountBalanceWallet } from "react-icons/md";
+
+const Card = () => {
+  return (
+    <div className="flex h-32 bg-gray-800 rounded-3xl p-4">
+        <div className="flex flex-col mx-2">
+          <p className="text-gray-400">Total balance</p>
+          <p className="text-white font-medium text-4xl">40000</p>
+        </div>
+        <div className="bg-red-500 w-12 h-12 rounded-full p-3 ml-auto my-[4%]">
+          <button className="cursor-pointer">
+            <MdAccountBalanceWallet className="text-2xl text-white" />
+          </button>
+        </div> 
+      </div>
+  )
+}
+
+export default Card
