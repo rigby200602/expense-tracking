583119a708d165fb9655730e04d0d91aa981c54a
rigby200602 <tungle3456@gmail.com>
2026-07-05 15:42:25 +0700

    feat: integrate Line and Doughnut charts for income and spending categories in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,75 @@
 import Card from "../components/Card";
-import { data } from "../assets/testData";
+import { data, overviewData, spendingCategory } from "../assets/testData";
+import { Doughnut, Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
 
-
-
+ChartJS.defaults.color = "#fff";
 
 const Home = () => {
   return (
     <div>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data.map((item) => (
-          <Card key={item.label} label={item.label} num={item.num} Icon={item.Icon} />
+          <Card
+            key={item.label}
+            label={item.label}
+            num={item.num}
+            Icon={item.Icon}
+          />
         ))}
+      </div>
+      <div className="flex gap-4 mt-4 text-white">
+        <div className="w-2/3 bg-gray-800 rounded-3xl p-4">
+          <Line
+            data={{
+              labels: overviewData.map((item) => item.month),
+              datasets: [
+                {
+                  label: "Income",
+                  data: overviewData.map((item) => item.income),
+                  borderColor: "rgba(75, 192, 192, 1)",
+                  backgroundColor: "rgba(75, 192, 192, 0.2)",
+                  fill: true,
+                },
+                {
+                  label: "Expenses",
+                  data: overviewData.map((item) => item.expenses),
+                  borderColor: "rgba(255, 99, 132, 1)",
+                  fill: true,
+                  backgroundColor: "rgba(255, 99, 132, 0.2)",
+                },
+              ],
+            }}
+          />
+        </div>
+        <div className="w-1/3 bg-gray-800 rounded-3xl p-4">
+          <Doughnut
+            data={{
+              labels: spendingCategory.map((item) => item.name),
+              datasets: [
+                {
+                  label: "Spending Category",
+                  data: spendingCategory.map((item) => item.amount),
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                  ],
+                  borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            }}
+          />
+        </div>
       </div>
     </div>
   );
