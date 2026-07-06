db7f84de29ec2e1fdb080f78e7ca20286eae0d84
rigby200602 <tungle3456@gmail.com>
2026-07-06 22:20:09 +0700

    fix: adjust grid layout in Home component for improved responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ ChartJS.defaults.color = "#fff";
 const Home = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {data.map((item) => (
           <Card
             key={item.label}
