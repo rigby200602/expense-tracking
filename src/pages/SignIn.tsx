84bd8d08d3fb9c4945e1191b7d983aa72a78e87d
rigby200602 <tungle3456@gmail.com>
2026-07-08 20:45:28 +0700

    feat: implement SignIn component layout with responsive design

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,11 @@
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className="flex justify-center items-center text-white h-screen my-auto">
+      <div className="bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/4 h-4/5 ">
+
+      </div>
+    </div>
   )
 }
 
