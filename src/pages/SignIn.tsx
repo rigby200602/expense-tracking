3349c783391b6975f42ba459fd288e497711c0c8
rigby200602 <tungle3456@gmail.com>
2026-07-08 20:49:57 +0700

    fix: add padding to SignIn component for improved layout

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,8 +2,8 @@
 const SignIn = () => {
   return (
     <div className="flex justify-center items-center text-white h-screen my-auto">
-      <div className="bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/4 h-4/5 ">
-
+      <div className="bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/4 h-4/5 py-[2%]">
+        <p className="text-center text-3xl font-bold">Sign in</p>
       </div>
     </div>
   )
