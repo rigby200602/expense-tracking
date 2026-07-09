137dd42093c3bd276bad2dd8750f721f0001107f
rigby200602 <tungle3456@gmail.com>
2026-07-09 20:53:24 +0700

    feat: update SignIn component layout for improved responsiveness

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,8 +1,8 @@
 
 const SignIn = () => {
   return (
-    <div className="flex justify-center items-center text-white h-screen my-auto">
-      <div className="bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/4 h-4/5 py-[2%]">
+    <div className="hidden lg:flex justify-center items-center text-white h-screen my-auto">
+      <div className="bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/3 h-4/5 py-[2%]">
         <p className="text-center text-3xl font-bold">Sign in</p>
       </div>
     </div>
