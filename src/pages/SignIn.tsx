36661363d95ded8cd33f7feff0dbd227324aede1
rigby200602 <tungle3456@gmail.com>
2026-07-09 21:38:53 +0700

    feat: add gap between input fields in SignIn component for improved spacing

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,13 +3,26 @@ const SignIn = () => {
     <div className="hidden lg:flex justify-center items-center text-white h-screen my-auto">
       <div className="flex flex-col bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/3 h-4/5 py-[2%]">
         <p className="text-center text-3xl font-bold">Sign in</p>
-        <div className="flex flex-col h-full my-[10%] mx-[10%] text-xl">
+        <div className="flex flex-col h-full my-[10%] mx-[10%] text-xl gap-[15%]">
           <div className="w-full">
             <p className="mb-2 text-gray-400">Email:</p>
             <input
+              id="email"
               type="email"
               placeholder="Email"
               title="Enter your email"
+              autoComplete="email"
+              className="rounded-xl px-4 bg-[#131A24] w-full h-full outline-0 autofill:bg-amber-900"
+            />
+          </div>
+          <div className="w-full">
+            <p className="mb-2 text-gray-400">Password:</p>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              title="Enter your password"
+              autoComplete="off"
               className="rounded-xl px-4 bg-[#131A24] w-full h-full outline-0"
             />
           </div>
