2a684e722fa42c8d21fc464b4d0802658def8091
rigby200602 <tungle3456@gmail.com>
2026-07-10 21:33:44 +0700

    feat: add onClick func

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -20,7 +20,8 @@ const SignIn = () => {
               autoComplete="off"
               className="rounded-4xl px-4 bg-[#131A24] w-full h-[15%] outline-0"
             />
-            <button className="bg-blue-600 hover:bg-blue-300 hover:text-black font-medium h-[15%] w-full rounded-4xl px-4 cursor-pointer">Sign in</button>
+            <button onClick={() => {}}
+            className="bg-blue-600 hover:bg-blue-300 hover:text-black font-medium h-[15%] w-full rounded-4xl px-4 cursor-pointer">Sign in</button>
           </div>
         </div>
       </div>
