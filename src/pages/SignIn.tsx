dcc2cd849b68799313657e70e471f100007b3d55
rigby200602 <tungle3456@gmail.com>
2026-07-10 21:27:06 +0700

    update: css

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,34 +3,26 @@ const SignIn = () => {
     <div className="hidden lg:flex justify-center items-center text-white h-screen my-auto">
       <div className="flex flex-col bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/3 h-4/5 py-[2%]">
         <p className="text-center text-3xl font-bold">Sign in</p>
-        <div className="flex flex-col h-full my-[10%] mx-[10%] text-xl gap-[15%]">
-          <div className="w-full">
+        <div className="flex flex-col h-full my-[10%] mx-[10%] text-xl gap-4">
             <input
               id="email"
               type="email"
               placeholder="Email"
               title="Enter your email"
               autoComplete="email"
-              className="block w-full py-2.5 px-0 text-sm text-white bg-transparent border-0 border-gray-300
-              appearance-none focus:outline-none focus:ring-0 focus:text-white peer"
+              className="rounded-xl px-4 bg-[#131A24] w-full h-[15%] outline-0"
             />
-            <label htmlFor="">Email</label>l
-          </div>
-          <div className="w-full">
             <input
               id="password"
               type="password"
               placeholder="Password"
               title="Enter your password"
               autoComplete="off"
-              className="block w-full py-2.5 px-0 text-sm text-white bg-transparent border-0 border-gray-300
-              appearance-none focus:outline-none focus:ring-0 focus:text-white peer"
+              className="rounded-xl px-4 bg-[#131A24] w-full h-[15%] outline-0"
             />
-            <label htmlFor="">Password</label>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
