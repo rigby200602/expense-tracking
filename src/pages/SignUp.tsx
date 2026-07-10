8c64b82c751e82cc2ca39dbbe71573ced8daae27
rigby200602 <tungle3456@gmail.com>
2026-07-10 21:37:02 +0700

    feat: copy layout

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,7 +2,7 @@ const SignIn = () => {
   return (
     <div className="hidden lg:flex justify-center items-center text-white h-screen my-auto">
       <div className="flex flex-col bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/3 h-4/5 py-[2%]">
-        <p className="text-center text-3xl font-bold">Sign in</p>
+        <p className="text-center text-3xl font-bold">Sign up</p>
         <div className="flex flex-col h-full my-[10%] mx-[10%] text-xl gap-4">
             <input
               id="email"
@@ -29,7 +29,7 @@ const SignIn = () => {
               className="rounded-4xl px-4 bg-[#131A24] w-full h-[15%] outline-0"
             />
             <button onClick={() => {}}
-            className="bg-blue-600 hover:bg-blue-300 hover:text-black font-medium h-[15%] w-full rounded-4xl px-4 cursor-pointer">Sign in</button>
+            className="bg-blue-600 hover:bg-blue-300 hover:text-black font-medium h-[15%] w-full rounded-4xl px-4 cursor-pointer">Sign up</button>
           </div>
         </div>
       </div>
