7eeb643a0310139edcc23e58beae7b28019aa201
rigby200602 <tungle3456@gmail.com>
2026-07-16 20:20:05 +0700

    feat: 1st html protocol

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,7 @@
 import express, { type Express, type Response, type Request} from 'express'
+
+const app: Express = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hi')
+})
