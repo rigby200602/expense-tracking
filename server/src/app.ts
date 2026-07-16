2df5b1fb9163e87add2e00f37dae3a0670978a8c
rigby200602 <tungle3456@gmail.com>
2026-07-16 20:33:01 +0700

    init backend and install dependencies

--- server/src/app.ts
+++
@@ -1,7 +0,0 @@
-import express, { type Express, type Response, type Request} from 'express'
-
-const app: Express = express()
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hi')
-})
