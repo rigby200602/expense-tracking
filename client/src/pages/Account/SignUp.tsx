744a2afd07407238abda96f21e603834621d8a22
rigby200602 <tungle3456@gmail.com>
2026-07-19 17:23:25 +0700

    update: change div to form

--- client/src/pages/Account/SignUp.tsx
+++ client/src/pages/Account/SignUp.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const SignIn = () => {
   return (
     <div className="hidden lg:flex justify-center items-center text-white h-screen my-auto">
-      <div className="flex flex-col bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/3 h-4/5 py-[2%]">
+      <form className="flex flex-col bg-[#0A0F16] border border-gray-700 rounded-2xl shadow-2xl w-1/3 h-4/5 py-[2%]">
         <p className="text-center text-3xl font-bold">Sign up</p>
         <div className="flex flex-col h-full m-[10%] text-xl gap-4">
             <input
@@ -36,7 +36,7 @@ const SignIn = () => {
             <button onClick={() => {}}
             className="bg-blue-600 hover:bg-blue-300 hover:text-black font-medium h-[15%] w-full rounded-4xl px-4 cursor-pointer">Sign up</button>
           </div>
-        </div>
+        </form>
       </div>
   );
 };
