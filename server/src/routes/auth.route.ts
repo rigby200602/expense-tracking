9b45f7cd2fb79a4481a984b6ccea9e2b48589ff1
rigby200602 <tungle3456@gmail.com>
2026-07-31 20:46:15 +0700

    update cmt

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { AuthController } from '../controllers/auth.controller.ts'
-// Public routes
-const authRouter = Router()
 
+const authRouter = Router()
+// Public routes
 authRouter.post('/register', AuthController.register)
 authRouter.post('/login', AuthController.login)
+// Protected routes
 authRouter.post('/logout', AuthController.logout)
 authRouter.get('/profile', AuthController.profile)
 
