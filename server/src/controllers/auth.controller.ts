90e4952747d5e5f46da9fb9a883ac2f2eb683ba5
rigby200602 <tungle3456@gmail.com>
2026-07-31 20:42:05 +0700

    proto type controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,7 +3,19 @@ import type { Request, Response } from "express"
 export const register = (req: Request, res: Response) => {
     return res.json({ message: "Register endpoint" })
 }
+export const login = (req: Request, res: Response) => {
+    return res.json({ message: "Login endpoint" })
+}
+export const logout = (req: Request, res: Response) => {
+    return res.json({ message: "Logout endpoint" })
+}
+export const profile = (req: Request, res: Response) => {
+    return res.json({ message: "Profile endpoint" })
+}
 
 export const AuthController = {
-    register
+    register,
+    login,
+    logout,
+    profile
 }
