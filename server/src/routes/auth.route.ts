1c4ea66cffd5df22667cc3081fb4cc33bd44e6e0
rigby200602 <tungle3456@gmail.com>
2026-07-31 20:18:20 +0700

    create route base on the design

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,3 +1,6 @@
 import { Router } from 'express'
 import AuthController from '../controllers/auth.controller.ts'
+// Route for auth
+const authRouter = Router()
 
+authRouter.post('/register', AuthController)
