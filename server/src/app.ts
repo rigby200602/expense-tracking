5f768bbe50d5166d723ed92a8925e1c1caeafbc1
rigby200602 <tungle3456@gmail.com>
2026-08-01 15:24:48 +0700

    route for auth

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,13 @@
 import express, { type Express, type Request, type Response } from 'express';
+import authRouter from './routes/auth.route.ts';
 
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+app.use('/api/auth', authRouter);
 
 app.listen(3000);
 
