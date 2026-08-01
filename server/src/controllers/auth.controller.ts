7b519f471b7ffc5f3efb6f0d02f900e7ee9ac3d0
rigby200602 <tungle3456@gmail.com>
2026-08-01 22:23:21 +0700

    update controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,14 +1,23 @@
 import type { Request, Response } from "express"
+import { registerService, loginService, logoutService, profileService } from "../services/auth.service.ts"
 
 export const register = (req: Request, res: Response) => {
-    return res.json({ message: "Register endpoint" })
+    const result = registerService()
+
+    return res.status(200).json(result)
 }
 export const login = (req: Request, res: Response) => {
-    return res.json({ message: "Login endpoint" })
+    const result = loginService()
+
+    return res.status(200).json(result)
 }
 export const logout = (req: Request, res: Response) => {
-    return res.json({ message: "Logout endpoint" })
+    const result = logoutService()
+
+    return res.status(200).json(result)
 }
 export const profile = (req: Request, res: Response) => {
-    return res.json({ message: "Profile endpoint" })
+    const result = profileService()
+
+    return res.status(200).json(result)
 }
