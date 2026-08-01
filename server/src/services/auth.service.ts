d1d575e10ffe92357e2e6737d5fa41b4661f0d6f
rigby200602 <tungle3456@gmail.com>
2026-08-01 22:09:25 +0700

    init auth services

---
+++ server/src/services/auth.service.ts
@@ -0,0 +1,12 @@
+export const registerService = () => {
+    return { message: "Register service" }
+}
+export const loginService = () => {
+    return { message: "Login service" }
+}
+export const logoutService = () => {
+    return { message: "Logout service" }
+}
+export const profileService = () => {
+    return { message: "Profile service" }
+}
