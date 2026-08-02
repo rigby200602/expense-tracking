637b28bec194aa5761a3cec05a6f7613dd242559
rigby200602 <tungle3456@gmail.com>
2026-08-02 19:54:18 +0700

    init auth repository

---
+++ server/src/repositories/auth.repository.ts
@@ -0,0 +1,29 @@
+export const createUser = () => {
+    return {
+        message: "User created successfully",
+    }
+}
+
+export const findUserByEmail = () => {
+    return {
+        message: "User found successfully",
+    }
+}
+
+export const findUserById = () => {
+    return {
+        message: "User found successfully",
+    }
+}
+
+export const updateUser = () => {
+    return {
+        message: "User updated successfully",
+    }
+}
+
+export const deleteUser = () => {
+    return {
+        message: "User deleted successfully",
+    }
+}
