6c72131fdcdb8fddd2b211b25fa9a10911cfb1d1
rigby200602 <tungle3456@gmail.com>
2026-08-02 19:27:20 +0700

    add cmt

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,21 +1,25 @@
 import type { Request, Response } from "express"
 import { registerService, loginService, logoutService, profileService } from "../services/auth.service.ts"
 
+// POST: Register
 export const register = (req: Request, res: Response) => {
     const result = registerService()
 
     return res.status(200).json(result)
 }
+// POST: Login
 export const login = (req: Request, res: Response) => {
     const result = loginService()
 
     return res.status(200).json(result)
 }
+// POST: Logout
 export const logout = (req: Request, res: Response) => {
     const result = logoutService()
 
     return res.status(200).json(result)
 }
+// GET: Profile
 export const profile = (req: Request, res: Response) => {
     const result = profileService()
 
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -3,10 +3,10 @@ import { register,login,logout,profile } from '../controllers/auth.controller.ts
 
 const authRouter = Router()
 // Public routes
-authRouter.post('/register', register)
-authRouter.post('/login', login)
+authRouter.post('/register', register) // POST: Register
+authRouter.post('/login', login) // POST: Login
 // Protected routes
-authRouter.post('/logout', logout)
-authRouter.get('/profile', profile)
+authRouter.post('/logout', logout) // POST: Logout
+authRouter.get('/profile', profile) // GET: Profile
 
 export default authRouter
