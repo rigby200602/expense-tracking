528b6448fb219393ca45f2d5c9f2abe0b352bf89
rigby200602 <tungle3456@gmail.com>
2026-08-13 20:48:43 +0700

    create user

--- server/src/test-prisma.ts
+++ server/src/test-prisma.ts
@@ -1,16 +1,27 @@
 import { prisma } from "./config/prisma.ts";
 
-async function testPrisma() {
+async function main() {
   try {
+    const user = await prisma.users.create({
+      data: {
+        name: "Tung 2",
+        email: "tung2@test.com",
+        password: "123456",
+      },
+    });
+
+    console.log("User created:");
+    console.log(user);
+
     const users = await prisma.users.findMany();
 
-    console.log("Database connected!");
+    console.log("All users:");
     console.log(users);
   } catch (error) {
-    console.error("Database connection failed:", error);
+    console.error("Database error:", error);
   } finally {
     await prisma.$disconnect();
   }
 }
 
-testPrisma();
+main();
