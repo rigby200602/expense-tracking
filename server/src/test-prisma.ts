2d22b09ab3f4503ff63fafd20a0cc84314b32475
rigby200602 <tungle3456@gmail.com>
2026-08-13 22:07:08 +0700

    find user api

--- server/src/test-prisma.ts
+++ server/src/test-prisma.ts
@@ -3,16 +3,22 @@ import { prisma } from "./config/prisma.ts";
 async function main() {
   try {
     // add a new user to the database
-    const user = await prisma.users.create({
-      data: {
-        name: "Tung 2",
-        email: "tung2@test.com",
-        password: "123456",
+    // const user = await prisma.users.create({
+    //   data: {
+    //     name: "Tung 2",
+    //     email: "tung2@test.com",
+    //     password: "123456",
+    //   },
+    // });
+    // find users in the database
+    const foundUser = await prisma.users.findUnique({
+      where: {
+        email: "tung@test.com",
       },
     });
 
-    console.log("User created:");
-    console.log(user);
+    console.log("Found user:");
+    console.log(foundUser);
 
     const users = await prisma.users.findMany();
 
